972f5a241e56dd495d8efac314d1f241831a00a8
khloud54 <kh.t705042000@gmail.com>
2024-02-18 01:43:00 -0800

    ww

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-""" FizzBuzz """
-
+""" FizzBuzz
+"""
 import sys
+
 
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
 
-    - If a number is divisible by three, it prints "Fizz" instead of
-    the number.
-    - If a number is divisible by five, it prints "Buzz" instead of
-    the number.
-    - If a number is divisible by both three and five, it prints "FzzBuzz".
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
     """
-
     if n < 1:
         return
 
-    output = []
+    tmp_result = []
     for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            output.append("FizzBuzz")
-        elif i % 3 == 0:
-            output.append("Fizz")
-        elif i % 5 == 0:
-            output.append("Buzz")
+        if (i % 3) == 0 and (i % 5) == 0:
+                tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+                tmp_result.append("Buzz")
+       
         else:
-             output.append(str(i))
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
-    print(" ".join(output))
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
@@ -37,10 +36,5 @@ if __name__ == '__main__':
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    try:
-        number = int(sys.argv[1])
-    except ValueError:
-        print("Invalid number")
-        sys.exit(1)
-
+    number = int(sys.argv[1])
     fizzbuzz(number)
